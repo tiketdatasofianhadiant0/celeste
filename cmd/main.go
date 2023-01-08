2d1bd2be87350c31dd54362ff90d8d7df4454d88
Risal <risal.risal@tiket.com>
2023-01-08 23:55:52 +0700

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tiketdatarisal/celeste"
+)
 
+const (
+	q1 = `
+{
+    "source": "people",
+    "find": {"$and": [{"name": "\"james\""}, {"age": {"$gt": 20}}]}
+}`
+	q2 = `
+{
+    "source": "people",
+    "find": {"$and": [{"name": {"$not_like": "\"%james%\""}}]}
+}`
+	q3 = `
+{
+    "source": "people",
+    "find": {"$and": [{"name": {"$in": ["\"james\"", "\"bond\""]}}]}
+}`
+)
+
+func main() {
+	q := celeste.Query{}
+	if err := json.Unmarshal([]byte(q3), &q); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(q)
 }
